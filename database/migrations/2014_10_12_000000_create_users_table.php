af49fe4a7e09abbec4f1aca245ec4d4b525a7b72
Rizky Septian <rizkyseptian018@gmail.com>
2023-12-14 18:06:02 +0700

    feat(authentification): add user authentification feature

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,9 +17,9 @@ return new class extends Migration
             $table->string('name');
             $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->integer('phone');
-            $table->string('address');
-            $table->integer('bank_account');
+            $table->integer('phone')->nullable();
+            $table->string('address')->nullable();
+            $table->integer('bank_account')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
